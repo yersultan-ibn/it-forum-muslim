9d37dedec2090ac1c0655c97d4c302e8856da13f
Yersultan <ersultansansyzbaj012@gmail.com>
2023-10-11 13:40:36 +0600

    fix: fix active-modal

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const modal = document.querySelector(".modal-container");
-const active = document.getElementById("active-modal");
+const active = document.querySelectorAll("#active-modal");
 window.addEventListener("click", (e) => {
   console.log(e.target);
-  if (e.target === active) {
-    modal.classList.add("display");
-    document.body.classList.add("hidden");
-  }
-  if (e.target === modal) {
-    modal.classList.remove("display");
-    document.body.classList.remove("hidden");
-  }
+  active.forEach((item) => {
+    if (e.target === item) {
+      modal.classList.add("display");
+      document.body.classList.add("hidden");
+    }
+    if (e.target === modal) {
+      modal.classList.remove("display");
+      document.body.classList.remove("hidden");
+    }
+  });
 });
