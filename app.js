b03c2c10a3457e0f56897f8a413c3c8200936e69
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-10-17 23:25:48 +0600

    fix: modal styles

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const modal = document.querySelector(".modal-container");
 const active = document.querySelectorAll("#active-modal");
+const modalCloseBtn = document.querySelector("#close-modal-btn")
 window.addEventListener("click", (e) => {
-  console.log(e.target);
   active.forEach((item) => {
     if (e.target === item) {
       modal.classList.add("display");
@@ -13,3 +13,8 @@ window.addEventListener("click", (e) => {
     }
   });
 });
+
+modalCloseBtn.addEventListener("click", () => {
+  modal.classList.remove("display");
+  document.body.classList.remove("hidden");
+})
